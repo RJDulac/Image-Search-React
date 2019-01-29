2a4e6d1d3acf0ef56eb684ed7843de350850a71e
Ryan Dulac <analyticrye@gmail.com>
2019-01-29 00:47:01 -0500

    cleaned up code and added keys

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = props => {
-  const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+  const images = props.images.map(({ description, urls, id }) => {
+    return <img src={urls.regular} key={id} alt={description} />;
   });
   return <div>{images}</div>;
 };
